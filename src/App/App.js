b8e5289ad4a83bbbb72ba5c64c7f64406fac85c9
Luis Ortiz <ortizlu@icloud.com>
2018-10-22 15:52:11 -0400

    added home component

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import Header from '../Header/Header';
+import React, { Component } from 'react'
+import './App.css'
+import Header from '../Header/Header'
+import Home from '../Home/Home'
+import { Link, Route, Switch, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-
+      <div>
         <Header />
-
-    );
+        <main>
+          <Switch>
+            <Route path="/" render={props => <Home />} />
+          </Switch>
+        </main>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h1>Where in the World is the ISS?</h1>
+        <div>
+          <img src="https://via.placeholder.com/500x500" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
