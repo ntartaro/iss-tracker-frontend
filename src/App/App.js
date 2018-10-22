cdd7ff96440a201b9e3e5facedb5388d00156c91
Nick Tartaro <ntartaro@gmail.com>
2018-10-22 15:12:50 -0400

    started header

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello ISS</h1>
-    )
+
+        <Header />
+
+    );
   }
 }
 
