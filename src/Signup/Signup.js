59ae734bea97c571b66516fbb78682dcdb190c2a
Nick Tartaro <ntartaro@gmail.com>
2018-10-23 15:14:43 -0400

    signup page draft

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import './Signup.css'
+import './Signup.css';
 
 class Signup extends Component {
-    render() {
-        return (
-            <section className='signup-wrapper'>
-                <div className='signup-top'>
-                    <p>Sign Up</p>
-                </div>
-                <div className='signup-main'>
-
-                </div>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="signup-wrapper">
+        <div className="signup-top">
+          <p>Sign Up</p>
+        </div>
+        <div className="signup-main">
+          <p>Username:</p>
+          <input type="text" />
+          <p>Password:</p>
+          <input type="password" />
+          <div className="signup-button-wrapper">
+            <button className="signup-button">SUBMIT</button>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Signup;
