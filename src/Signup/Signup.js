c94ba2b213d104659418ab3648c9bc47d041a79c
Nick Tartaro <ntartaro@gmail.com>
2018-10-23 16:43:47 -0400

    signup state

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -4,7 +4,27 @@ import './Signup.css';
 class Signup extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+        username: '',
+        password: ''
+    }
+    this.handleChangeUsername = this.handleChangeUsername.bind(this)
+    this.handleChangePassword = this.handleChangePassword.bind(this)
   }
+
+  handleChangeUsername(event) {
+      this.setState({
+          username: event.target.value
+      })
+      console.log(this.state.username)
+  }
+
+    handleChangePassword(event) {
+        this.setState({
+            password: event.target.value
+        })
+        console.log(this.state.password)
+    }
 
   render() {
     return (
@@ -13,11 +33,11 @@ class Signup extends Component {
           <p>Sign Up</p>
         </div>
         <div className="signup-main">
-          <form className='signup-main-wrapper'>
+          <form className="signup-main-wrapper">
             <label>Username:</label>
-            <input type="text" />
+            <input type="text" value={this.state.username} onChange={this.handleChangeUsername} />
             <label>Password:</label>
-            <input type="password" />
+            <input type="password" value={this.state.password} onChange={this.handleChangePassword}/>
           </form>
           <div className="signup-button-wrapper">
             <button className="signup-button">SIGN UP</button>
