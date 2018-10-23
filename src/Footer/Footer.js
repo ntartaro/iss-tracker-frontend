b3596ab585cfb975e936f998b26a4eff4b7f7e28
Nick Tartaro <ntartaro@gmail.com>
2018-10-22 23:21:25 -0400

    added github logos in footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import './Footer.css'
+import './Footer.css';
 
 class Footer extends Component {
-    render() {
-        return (
-           <footer>
-               <div>
-                   <p>footer</p>
-               </div>
-           </footer>
-        );
-    }
+  render() {
+    return (
+      <footer>
+        <div />
+        <div>
+          <ul>
+            <li>
+              <a>
+                <img src="/images/github_logo.png" />
+                <p>ntartaro</p>
+              </a>
+            </li>
+            <li>
+              <a>
+                <img src="/images/github_logo.png" />
+                <p>ortizlu</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
