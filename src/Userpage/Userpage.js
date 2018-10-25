3a48e405f2d3a345c1b7b14005fd729218833e7a
Luis Ortiz <ortizlu@icloud.com>
2018-10-25 19:11:29 -0400

    got one location to show

--- src/Userpage/Userpage.js
+++ src/Userpage/Userpage.js
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
-import './Userpage.css';
-import { Link } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
+import React, { Component } from 'react'
+import './Userpage.css'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 class Userpage extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       user: {
         username: '',
         savedLocations: []
       }
-    };
+    }
   }
 
   userShow = () => {
-    // if (localStorage.token) {
-    axios
-      .get('http://localhost:3001/users/' + this.props.match.params.id, {
-        headers: {
-          Authorization: localStorage.token
-        }
-      })
-      .then(response => {
-        console.log(response);
-        this.setState({
-          user: response.data
-        });
-      });
-  };
-  // };
+    let userid = jwtDecode(localStorage.token).id
+    if (localStorage.token) {
+      axios
+        .get('http://localhost:3001/users/' + userid, {
+          headers: {
+            Authorization: localStorage.token
+          }
+        })
+        .then(response => {
+          this.setState({
+            user: response.data
+          })
+        })
+    }
+  }
 
   componentDidMount() {
-    this.userShow();
+    this.userShow()
   }
 
   render() {
-    let name;
-    if (localStorage.token) {
-      name = jwtDecode(localStorage.token).username;
-    }
     return (
       <section>
         <div className="user-header-wrapper">
@@ -55,61 +51,58 @@ class Userpage extends Component {
             <ul>
               <Link
                 className="new-location-link"
-                to={'/user/' + name + '/newlocation'}
+                to={'/user/' + this.props.match.params.id + '/location/new'}
               >
                 <li>New Location</li>
               </Link>
-              <Link className="edit-user-link" to={'/user/' + name + '/edit'}>
+              <Link
+                className="edit-user-link"
+                to={'/user/' + this.props.match.params.id + '/edit'}
+              >
                 <li>Edit User</li>
               </Link>
             </ul>
           </div>
           <div className="location-wrapper">
-            {this.state.user ? (
-              this.state.user.savedLocations.map(location => {
-                return (
-                  <div className="location-card">
-                    <Link
-                      to={
-                        '/user/' +
-                        this.props.match.params.id +
-                        '/location/' +
-                        location._id
-                      }
-                    >
-                      <img src="/images/staticmap.png" alt="location1" />
-                    </Link>
-                    <div className="bottom-card">
-                      <p>{location.title}</p>
-                      <div className="location-button-wrapper">
-                        <Link
-                          to={
-                            '/user/' +
-                            this.props.match.params.id +
-                            '/location/' +
-                            location._id +
-                            '/edit'
-                          }
-                        >
-                          <button className="location-edit-button">EDIT</button>
-                        </Link>
+            {this.state.user.savedLocations.map(location => {
+              return (
+                <div key={location._id} className="location-card">
+                  <Link
+                    to={
+                      '/user/' +
+                      this.props.match.params.id +
+                      '/location/' +
+                      location._id
+                    }
+                  >
+                    <img src="/images/staticmap.png" alt="location1" />
+                  </Link>
+                  <div className="bottom-card">
+                    <p>{location.title}</p>
+                    <div className="location-button-wrapper">
+                      <Link
+                        to={
+                          '/user/' +
+                          this.props.match.params.id +
+                          '/location/' +
+                          location._id +
+                          '/edit'
+                        }
+                      >
+                        <button className="location-edit-button">EDIT</button>
+                      </Link>
 
-                        <button className="location-delete-button">
-                          DELETE
-                        </button>
-                      </div>
+                      <button className="location-delete-button">DELETE</button>
                     </div>
                   </div>
-                );
-              })
-            ) : (
-              <div />
-            )}
+                </div>
+              )
+            })}
           </div>
         </div>
       </section>
-    );
+    )
   }
 }
 
-export default Userpage;
+export default Userpage
