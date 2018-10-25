288268c62722560e6266b2251d20d9e9effb16c1
Nick Tartaro <ntartaro@gmail.com>
2018-10-25 13:52:39 -0400

    userpage reworks

--- src/Userpage/Userpage.js
+++ src/Userpage/Userpage.js
@@ -1,49 +1,92 @@
-import React, { Component } from 'react'
-import './Userpage.css'
-import { Link } from 'react-router-dom'
-import jwtDecode from 'jwt-decode'
+import React, { Component } from 'react';
+import './Userpage.css';
+import { Link } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 class Userpage extends Component {
   render() {
-    let name
+    let name;
     if (localStorage.token) {
-      name = jwtDecode(localStorage.token).username
+      name = jwtDecode(localStorage.token).username;
     }
     return (
-      <div className="grid-container">
-        <div className="user-settings">
-          <p>User Settings</p>
-          <ul>
-            <Link
-              className="new-location-link"
-              to={'/user/' + name + '/newlocation'}
-            >
-              <li>New Location</li>
-            </Link>
-            <Link className="edit-user-link" to={'/user/' + name + '/edit'}>
-              <li>Edit User</li>
-            </Link>
-          </ul>
+      <section>
+        <div className="user-header-wrapper">
+          <div className="user-header">
+            <p>User Locations</p>
+          </div>
         </div>
-        <div className="user-locations">
-          <p>User Locations</p>
-        </div>
-        {this.props.user ? (
-          this.props.user.savedLocations.map(location => (
-            <div className="location one">
-              <p>Test Title</p>
-              <img src="/images/staticmap.png" alt='location1' />
-              <div className="location-button-wrapper">
-                <button>Edit</button>
-                <button>Delete</button>
+
+        <div className="user-wrapper">
+          <div className="user-settings">
+            <p>User Settings</p>
+            <ul>
+              <Link
+                className="new-location-link"
+                to={'/user/' + name + '/newlocation'}
+              >
+                <li>New Location</li>
+              </Link>
+              <Link className="edit-user-link" to={'/user/' + name + '/edit'}>
+                <li>Edit User</li>
+              </Link>
+            </ul>
+          </div>
+          <div className="location-wrapper">
+            {/* {this.props.user ? (
+              this.props.user.savedLocations.map(location => ( */}
+
+            <div className="location-card">
+              <img src="/images/staticmap.png" alt="location1" />
+              <div className="bottom-card">
+                <p>Washington D.C.</p>
+                <div className="location-button-wrapper">
+                  <button className="location-edit-button">EDIT</button>
+                  <button className="location-delete-button">DELETE</button>
+                </div>
               </div>
             </div>
-          ))
-        ) : (
-          <div />
-        )}
-      </div>
-    )
+
+            <div className="location-card">
+              <img src="/images/staticmap.png" alt="location1" />
+              <div className="bottom-card">
+                <p>Los Angeles</p>
+                <div className="location-button-wrapper">
+                  <button className="location-edit-button">EDIT</button>
+                  <button className="location-delete-button">DELETE</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="location-card">
+              <img src="/images/staticmap.png" alt="location1" />
+              <div className="bottom-card">
+                <p>New York</p>
+                <div className="location-button-wrapper">
+                  <button className="location-edit-button">EDIT</button>
+                  <button className="location-delete-button">DELETE</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="location-card">
+              <img src="/images/staticmap.png" alt="location1" />
+              <div className="bottom-card">
+                <p>Los Angeles</p>
+                <div className="location-button-wrapper">
+                  <button className="location-edit-button">EDIT</button>
+                  <button className="location-delete-button">DELETE</button>
+                </div>
+              </div>
+            </div>
+            {/* ))
+            ) : (
+              <div />
+            )} */}
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
@@ -51,6 +94,6 @@ Userpage.defaultProps = {
   user: {
     savedLocations: []
   }
-}
+};
 
-export default Userpage
+export default Userpage;
